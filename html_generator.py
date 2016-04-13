fe1ababc459e6734c759782ff251b73b7321e084
KGS-Rastede <ni@kgs-rastede.de>
2016-04-13 21:17:22 +0200

    weniger Debug-Output und schreiben in die korrekte Anzahl von Dateien

--- html_generator.py
+++ html_generator.py
@@ -13,22 +13,28 @@ class html_generator():
     def erzeuge_html(self, regelungen):
         html_code = self.vorne
         counter = 0
+        dateinummer = 1
         for r in regelungen:
-            counter += 1
             html_code += self.erzeuge_html_zeile(r)
-            # print("drucke Regel")
 
-        #print(html_code)
-        html_code += self.hinten
-        self.schreibe_html(html_code, 2)
+            if counter % 10 is 0 and counter > 0:
+                html_code += self.hinten
+                self.schreibe_html(html_code, dateinummer)
+                dateinummer += 1
+                html_code = self.vorne # für neue Datei vorbereiten
+
+            counter += 1
+
+        # print("Regel {} wurde geschrieben".format(counter))
 
     def schreibe_html(self, html_code, nummer):
-        dateiname = "test"+str(nummer)+".htm"
+        print("Schreibe Datei Nummer "+str(nummer))
+        dateiname = "test_"+str(nummer)+".htm"
         with open(dateiname, 'w') as outf:
             outf.write(html_code)
 
     def erzeuge_html_zeile(self, regel):
         string = "<tr class=\'list odd\'><td class=\"list\" align=\"center\"><b>{}</b></td><td class=\"list\" align=\"center\"><b>{}</b></td><td class=\"list\" align=\"center\"><b>{}</b></td><td class=\"list\" align=\"center\"><b>{}</b></td><td class=\"list\" align=\"center\"><b>{}</b></td><td class=\"list\" align=\"center\">{}</td></tr>".format(regel.k, regel.s, regel.l, regel.kurs, regel.r, regel.v)
-        regel.debug()
-        print(string)
+        # regel.debug()
+        # print(string)
         return string
