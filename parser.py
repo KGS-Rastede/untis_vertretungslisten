9c063e35746e7d1c7252dcf163ae36977fc0da35
KGS-Rastede <ni@kgs-rastede.de>
2016-04-13 10:21:28 +0200

    Öffnen der Datei in einer Methode gepackt

--- parser.py
+++ parser.py
@@ -1,10 +1,15 @@
 from bs4 import BeautifulSoup
 
-html_doc_heute = open("subst_001.htm", 'r').read()
-html_doc_morgen = open("subst_002.htm", 'r').read()
+def erste_soups():
+    html_doc_heute = open("subst_001.htm", 'r').read()
+    html_doc_morgen = open("subst_002.htm", 'r').read()
 
+    s1 = BeautifulSoup(html_doc_heute, 'html.parser')
+    s2 = BeautifulSoup(html_doc_morgen, 'html.parser')
+    return s1, s2
 
-soup_heute  = BeautifulSoup(html_doc_heute, 'html.parser')
-soup_morgen = BeautifulSoup(html_doc_morgen, 'html.parser')
+soup_heute, soup_morgen = erste_soups()
+
 
 print(soup_heute.head.contents[1])
+print(soup_morgen.head.contents[1])
