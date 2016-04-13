4cd3bae6ca49bbe1764db39df7615377862ec3f5
KGS-Rastede <ni@kgs-rastede.de>
2016-04-13 20:32:20 +0200

    Erste Version des HTML_Generators

---
+++ html_generator.py
@@ -0,0 +1,9 @@
+class html_generator():
+    def __init__(self):
+        lade_html()
+
+    def lade_html(self):
+        with open('rumpfdatei_vorne.htm', 'r') as f:
+            vorne = f.read()
+        with open('rumpfdatei_hinten.htm', 'r') as f:
+            hinten = f.read()
