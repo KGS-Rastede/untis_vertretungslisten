6cc846fc642b78972dabb098bf82f6be6208033c
KGS-Rastede <ni@kgs-rastede.de>
2016-04-13 20:44:29 +0200

    Sinnvolle Variablen für den html-Code

--- html_generator.py
+++ html_generator.py
@@ -1,9 +1,15 @@
 class html_generator():
     def __init__(self):
+        self.vorne = ""
+        self.hinten = ""
         self.lade_html()
 
     def lade_html(self):
         with open('rumpfdatei_vorne.htm', 'r') as f:
-            vorne = f.read()
+            self.vorne = f.read()
         with open('rumpfdatei_hinten.htm', 'r') as f:
-            hinten = f.read()
+            self.hinten = f.read()
+
+    def erzeuge_html(self, regelungen):
+        pass
+        # print(self.vorne)
