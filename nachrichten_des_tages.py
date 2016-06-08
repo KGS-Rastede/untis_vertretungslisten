74e835938355fffd20f8637761fdb410214b6f4c
KGS-Rastede <ni@kgs-rastede.de>
2016-06-08 12:18:45 +0200

    NDT ausgelagert

--- nachrichten_des_tages.py
+++ nachrichten_des_tages.py
@@ -39,7 +39,7 @@ class NachrichtenDesTages():
         """
 
         """
-        html_code = "<tr class=\"info\"><th class=\"info\" align=\"center\" colspan=\"2\">Nachrichten zum Tag</th></tr>"
+        html_code = "<tr class=\"info\"><th class=\"info\" align=\"center\" colspan=\"4\">Nachrichten zum Tag</th></tr>"
 
         ndt = []
 
@@ -50,13 +50,28 @@ class NachrichtenDesTages():
 
         i = 0
 
+        anzahl_regelungen = len(ndt)
+
         for zeile in ndt:
             html_code += "<tr class=\"info\"><td class=\"info\" align=\"left\">"
             html_code += ndt[i][0]
             html_code += "</td>"
             html_code += "<td class=\"info\" align=\"left\">"
             html_code += ndt[i][1]
-            html_code += "</td></tr>"
+            html_code += "</td>"
+
+            if i < anzahl_regelungen:
+                html_code += "<td class=\"info\" align=\"left\">"
+                html_code += ndt[i][0]
+                html_code += "</td>"
+
+                html_code += "<td class=\"info\" align=\"left\">"
+                html_code += ndt[i][1]
+                html_code += "</td>"
+
+
+
+            html_code += "</tr>"
 
             i += 1
 
